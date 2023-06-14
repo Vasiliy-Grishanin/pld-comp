3ea821fb4e5544a38540d47772964142e51ca889
Antonin <antonin.soucasse@insa-lyon.fr>
2023-06-14 14:48:09 +0200

    makefile compatibility for Fedora

--- compiler/fedora.mk
+++ compiler/fedora.mk
@@ -1,3 +1,8 @@
 # required packages (fedora 34): antlr4 and antlr4-cpp-runtime-devel
-make ANTLRJAR=/usr/share/java/antlr4/antlr4.jar:/usr/share/java/antlr3-runtime.jar:/usr/share/java/antlr4/antlr4-runtime.jar:/usr/share/java/stringtemplate4/ST4.jar:/usr/share/java/treelayout/org.abego.treelayout.core.jar ANTLRINC=/usr/include/antlr4-runtime ANTLRLIB=/usr/lib64/libantlr4-runtime.so "$@"
+# make ANTLRJAR=/usr/share/java/antlr4/antlr4.jar:/usr/share/java/antlr3-runtime.jar:/usr/share/java/antlr4/antlr4-runtime.jar:/usr/share/java/stringtemplate4/ST4.jar:/usr/share/java/treelayout/org.abego.treelayout.core.jar ANTLRINC=/usr/include/antlr4-runtime ANTLRLIB=/usr/lib64/libantlr4-runtime.so "$@"
 
+# ANTLRJAR=/usr/share/java/stringtemplate4.jar:/usr/share/java/antlr4.jar:/usr/share/java/antlr4-runtime.jar:/usr/share/java/antlr3-runtime.jar/:/usr/share/java/treelayout.jar
+ANTLRINC=/usr/include/antlr4-runtime
+ANTLRLIB=/usr/lib64/libantlr4-runtime.so
+ANTLR=antlr4
+ANTLRJAR=/usr/share/java/antlr4/antlr4.jar:/usr/share/java/antlr4/antlr4-runtime.jar:/home/antonin/Téléchargements/org.abego.treelayout.core-1.0.3.jar
