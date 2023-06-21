faac8e5b9f9ba74a3f418210474d6fdf907d6850
arthur <arthur.luvison@icloud.com>
2023-06-21 17:35:38 +0200

    fix et ajout de teste

---
+++ tests/testfiles/valid/arithmetics/addition_division.c
@@ -0,0 +1,9 @@
+int main() {
+    int a = 1; 
+    int b = 2;
+    int c = 4; 
+    int d = 8; 
+    int result;
+    result = ((a + b) * (c - d)) + (c * d) - ((a + c) / (b - d));
+    return result;
+}
---
+++ tests/testfiles/valid/arithmetics/addition_subtraction_priority.c
@@ -0,0 +1,9 @@
+int main() {
+    int a = 5; 
+    int b = 2; 
+    int c = 3; 
+    int d = 4;
+    int result;
+    result = ((a + b) * (c - d)) - ((a + c) - (b * d));
+    return result;
+}
